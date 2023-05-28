1a00f2306fe386029aa4cb4127a333dd9b45a5d5
Jake Delaney <jakedelaney24@gmail.com>
2023-05-28 11:13:31 +0000

    Add function to display and iterate through question items of questionArray

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,145 +1,144 @@
-document.addEventListener("DOMContentLoaded", function(){
-  alert("DOM loaded")
-  shuffle(questionArray)
-  console.log(questionArray[0].question)
-})
+document.addEventListener("DOMContentLoaded", function() {
+  shuffle(questionArray);
+  displayQuestion();
+});
 
-startGame(){
+/*startGame(){
   displayQuestion()
-}
+}*/
 
 const quizQuestion = document.getElementById("question");
-const quizChoices = document.getElementsByClassName("answer--btn");
+const answerBtn = document.getElementsByClassName("answer--btn");
 const submitBtn = document.getElementById("submit--btn");
 const resetBtn = document.getElementById("reset--btn");
-let currentQuestiontIndex = 0; // keeps track of current question index
+let currentQuestionIndex = 0; // keeps track of current question index
 
 let questionArray = [ //creates an array of questions, possible answers, and a correct answer
     {
       question: "What is the capital city of Australia?",
-      possibleAnswers: ["Sydney", "Melbourne", "Canberra", "Perth"],
+      choices: ["Sydney", "Melbourne", "Canberra", "Perth"],
       correctAnswer: "Canberra"
     },
     {
       question: "Which continent is the largest in terms of land area?",
-      possibleAnswers: ["Africa", "Asia", "North America", "Europe"],
+      choices: ["Africa", "Asia", "North America", "Europe"],
       correctAnswer: "Asia"
     },
     {
       question: "What is the longest river in the world?",
-      possibleAnswers: ["Nile River", "Amazon River", "Yangtze River", "Mississippi River"],
+      choices: ["Nile River", "Amazon River", "Yangtze River", "Mississippi River"],
       correctAnswer: "Nile River"
     },
     {
       question: "Which country is known as the 'Land of the Rising Sun'?",
-      possibleAnswers: ["China", "India", "Japan", "South Korea"],
+      choices: ["China", "India", "Japan", "South Korea"],
       correctAnswer: "Japan"
     },
     {
       question: "What is the tallest mountain in the world?",
-      possibleAnswers: ["Mount Everest", "K2", "Kangchenjunga", "Mount Kilimanjaro"],
+      choices: ["Mount Everest", "K2", "Kangchenjunga", "Mount Kilimanjaro"],
       correctAnswer: "Mount Everest"
     },
     {
       question: "Which ocean is the largest in the world?",
-      possibleAnswers: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+      choices: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
       correctAnswer: "Pacific Ocean"
     },
     {
       question: "Which country is both in Europe and Asia?",
-      possibleAnswers: ["Russia", "Turkey", "Kazakhstan", "Armenia"],
+      choices: ["Russia", "Turkey", "Kazakhstan", "Armenia"],
       correctAnswer: "Russia"
     },
     {
       question: "What is the official language of Brazil?",
-      possibleAnswers: ["Spanish", "Portuguese", "English", "French"],
+      choices: ["Spanish", "Portuguese", "English", "French"],
       correctAnswer: "Portuguese"
     },
     {
       question: "Which African country is known as the 'Pearl of Africa'?",
-      possibleAnswers: ["Kenya", "Tanzania", "Uganda", "Ethiopia"],
+      choices: ["Kenya", "Tanzania", "Uganda", "Ethiopia"],
       correctAnswer: "Uganda"
     },
     {
       question: "Which city is located at the mouth of the Amazon River?",
-      possibleAnswers: ["Rio de Janeiro", "Lima", "Buenos Aires", "Belem"],
+      choices: ["Rio de Janeiro", "Lima", "Buenos Aires", "Belem"],
       correctAnswer: "Belem"
     },
     {
-        question: "Which country is known as the 'Land Down Under'?",
-        possibleAnswers: ["New Zealand", "Australia", "Argentina", "South Africa"],
-        correctAnswer: "Australia"
-      },
-      {
-        question: "What is the largest desert in the world?",
-        possibleAnswers: ["Sahara Desert", "Gobi Desert", "Arabian Desert", "Antarctic Desert"],
-        correctAnswer: "Sahara Desert"
-      },
-      {
-        question: "Which city is located on two continents?",
-        possibleAnswers: ["Istanbul", "Cairo", "Athens", "Rome"],
-        correctAnswer: "Istanbul"
-      },
-      {
-        question: "Which country is famous for its tulips and windmills?",
-        possibleAnswers: ["Germany", "Netherlands", "Denmark", "Sweden"],
-        correctAnswer: "Netherlands"
-      },
-      {
-        question: "Which is the smallest country in the world by land area?",
-        possibleAnswers: ["Vatican City", "Monaco", "San Marino", "Nauru"],
-        correctAnswer: "Vatican City"
-      },
-      {
-        question: "What is the national language of Japan?",
-        possibleAnswers: ["Japanese", "Mandarin", "Korean", "Thai"],
-        correctAnswer: "Japanese"
-      },
-      {
-        question: "Which river flows through the Grand Canyon?",
-        possibleAnswers: ["Mississippi River", "Colorado River", "Columbia River", "Yukon River"],
-        correctAnswer: "Colorado River"
-      },
-      {
-        question: "What is the largest city in Canada?",
-        possibleAnswers: ["Toronto", "Vancouver", "Montreal", "Calgary"],
-        correctAnswer: "Toronto"
-      },
-      {
-        question: "Which country is home to the famous Angkor Wat temple complex?",
-        possibleAnswers: ["Cambodia", "Thailand", "Vietnam", "Myanmar"],
-        correctAnswer: "Cambodia"
-      },
-      {
-        question: "Which mountain range is located in South America?",
-        possibleAnswers: ["Rocky Mountains", "Andes Mountains", "Himalayas", "Alps"],
-        correctAnswer: "Andes Mountains"
-      },
-      {
-        question: "Which African country is the most populous?",
-        possibleAnswers: ["Nigeria", "Egypt", "South Africa", "Ethiopia"],
-        correctAnswer: "Nigeria"
-      },
-      {
-        question: "What is the capital city of Canada?",
-        possibleAnswers: ["Toronto", "Montreal", "Ottawa", "Vancouver"],
-        correctAnswer: "Ottawa"
-      },
-      {
-        question: "Which country is known for the famous landmark, Machu Picchu?",
-        possibleAnswers: ["Mexico", "Brazil", "Peru", "Argentina"],
-        correctAnswer: "Peru"
-      },
-      {
-        question: "What is the largest lake in Africa?",
-        possibleAnswers: ["Lake Victoria", "Lake Tanganyika", "Lake Malawi", "Lake Chad"],
-        correctAnswer: "Lake Victoria"
-      },
-      {
-        question: "Which city is known as the 'Eternal City'?",
-        possibleAnswers: ["Paris", "Rome", "Madrid", "Athens"],
-        correctAnswer: "Rome"
-      }
+      question: "Which country is known as the 'Land Down Under'?",
+      choices: ["New Zealand", "Australia", "Argentina", "South Africa"],
+      correctAnswer: "Australia"
+    },
+    {
+      question: "What is the largest desert in the world?",
+      choices: ["Sahara Desert", "Gobi Desert", "Arabian Desert", "Antarctic Desert"],
+      correctAnswer: "Sahara Desert"
+    },
+    {
+      question: "Which city is located on two continents?",
+      choices: ["Istanbul", "Cairo", "Athens", "Rome"],
+      correctAnswer: "Istanbul"
+    },
+    {
+      question: "Which country is famous for its tulips and windmills?",
+      choices: ["Germany", "Netherlands", "Denmark", "Sweden"],
+      correctAnswer: "Netherlands"
+    },
+    {
+      question: "Which is the smallest country in the world by land area?",
+      choices: ["Vatican City", "Monaco", "San Marino", "Nauru"],
+      correctAnswer: "Vatican City"
+    },
+    {
+      question: "What is the national language of Japan?",
+      choices: ["Japanese", "Mandarin", "Korean", "Thai"],
+      correctAnswer: "Japanese"
+    },
+    {
+      question: "Which river flows through the Grand Canyon?",
+      choices: ["Mississippi River", "Colorado River", "Columbia River", "Yukon River"],
+      correctAnswer: "Colorado River"
+    },
+    {
+      question: "What is the largest city in Canada?",
+      choices: ["Toronto", "Vancouver", "Montreal", "Calgary"],
+      correctAnswer: "Toronto"
+    },
+    {
+      question: "Which country is home to the famous Angkor Wat temple complex?",
+      choices: ["Cambodia", "Thailand", "Vietnam", "Myanmar"],
+      correctAnswer: "Cambodia"
+    },
+    {
+      question: "Which mountain range is located in South America?",
+      choices: ["Rocky Mountains", "Andes Mountains", "Himalayas", "Alps"],
+      correctAnswer: "Andes Mountains"
+    },
+    {
+       question: "Which African country is the most populous?",
+      choices: ["Nigeria", "Egypt", "South Africa", "Ethiopia"],
+       correctAnswer: "Nigeria"
+    },
+    {
+      question: "What is the capital city of Canada?",
+      choices: ["Toronto", "Montreal", "Ottawa", "Vancouver"],
+      correctAnswer: "Ottawa"
+    },
+    {
+      question: "Which country is known for the famous landmark, Machu Picchu?",
+      choices: ["Mexico", "Brazil", "Peru", "Argentina"],
+      correctAnswer: "Peru"
+    },
+    {
+       question: "What is the largest lake in Africa?",
+       choices: ["Lake Victoria", "Lake Tanganyika", "Lake Malawi", "Lake Chad"],
+       correctAnswer: "Lake Victoria"
+    },
+    {
+      question: "Which city is known as the 'Eternal City'?",
+      choices: ["Paris", "Rome", "Madrid", "Athens"],
+      correctAnswer: "Rome"
+    }
   ];
 
 
@@ -159,7 +158,31 @@ function shuffle(questionArray){
   return questionArray;
 }
 
-shuffle(questionArray)
+function displayQuestion(){
+  const currentQuestion = questionArray[currentQuestionIndex];
+  const currentChoices = currentQuestion.choices;
+  console.log(currentQuestion)
+  console.log(currentChoices)
+
+  quizQuestion.innerHTML = currentQuestion.question;
+
+  for (let j = 0; j < currentChoices.length; j++){
+    answerBtn[j].innerHTML = currentChoices[j];
+  }
+}
 
 
+displayQuestion()
 
+function submitAnswer(){
+  if (currentQuestionIndex === 10){
+    alert("test")
+  }
+  currentQuestionIndex++;
+  displayQuestion()
+}
+
+
+submitBtn.addEventListener("click", submitAnswer)
+
+submitAnswer()
